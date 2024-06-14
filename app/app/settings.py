c6b6fd5fc722e62d50d647e8778d7fd5feb339ce
faizan1487 <faizanahmed14887@gmail.com>
2024-06-14 13:03:50 +0500

    recipe app created

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,11 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
     'rest_framework',
     'drf_spectacular',
-    'user',
     'rest_framework.authtoken',
+    'core',
+    'user',
+    'recipe',
 ]
 
 MIDDLEWARE = [
