81c87fb9d9756adde42f92a256a37e6de283bc15
faizan1487 <faizanahmed14887@gmail.com>
2024-06-15 19:25:59 +0500

    write tests for listing tags api

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -77,5 +77,4 @@ class ModelTests(TestCase):
         """Test creating a tag is successful."""
         user = create_user()
         tag = models.Tag.objects.create(user=user, name='Tag1')
-        print(tag)
         self.assertEqual(str(tag), tag.name)
