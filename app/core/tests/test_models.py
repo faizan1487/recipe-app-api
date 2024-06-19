24982087b1b35fff9f3a5d992c680e80b0134b0a
faizan1487 <faizanahmed14887@gmail.com>
2024-06-19 18:11:29 +0500

    ingredient model created

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -78,3 +78,13 @@ class ModelTests(TestCase):
         user = create_user()
         tag = models.Tag.objects.create(user=user, name='Tag1')
         self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test creating an ingredient is successful."""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            name='ingredient1',
+            user=user
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
