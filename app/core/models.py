7455b1237970ca263cbc1e95a24e7883923a012d
faizan1487 <faizanahmed14887@gmail.com>
2024-06-26 17:00:16 +0500

    image filed added in recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -18,6 +18,8 @@ def recipe_image_file_path(instance, filename):
     ext = os.path.splitext(filename)[1]
     filename = f'{uuid.uuid4()}{ext}'
     
+    return os.path.join('uploads', 'recipe', filename)
+    
     
 class UserManager(BaseUserManager):
     """Manager for users."""
@@ -67,6 +69,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
